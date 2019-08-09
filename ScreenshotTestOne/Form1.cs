26731478e8208a44d112a27509e7ad8d8ae06f7d
yzh <867609570@qq.com>
2019-08-09 17:19:28 +0800

    1.0 截图（类似QQ）

--- ScreenshotTestOne/Form1.cs
+++ ScreenshotTestOne/Form1.cs
@@ -218,7 +218,7 @@ namespace ScreenshotTestOne
             }
             else
             {
-                this.Cursor = Cursors.Default;
+                this.Cursor = Cursors.Default; 
             }
         }
         #endregion
